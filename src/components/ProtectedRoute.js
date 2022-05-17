04436d822cdc57e2031eb4c60dda95b6a6a0e6aa
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-05-18 05:21:16 +0530

    firestore database

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,14 +1,14 @@
-import React, { Children } from "react";
+import React from "react";
 import { Navigate } from "react-router-dom";
 import { UserAuth } from "../Context/AuthContext";
 
-const ProtectedRoute = () => {
+const ProtectedRoute = ({ children }) => {
   const { user } = UserAuth();
 
   if (!user) {
     return <Navigate to="/" />;
   } else {
-    return Children;
+    return children;
   }
 };
 
